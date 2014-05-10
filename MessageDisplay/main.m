d630835b17d8582be43263ca5fd6ac4cb4ab6c94
zhoubin@moshi <admin@zhoubinmoshi.local>
2014-05-10 11:01:02 +0800

    Initial Commit

---
+++ MessageDisplay/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MessageDisplay
+//
+//  Created by zhoubin@moshi on 14-5-10.
+//  Copyright (c) 2014年 Crius_ZB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
